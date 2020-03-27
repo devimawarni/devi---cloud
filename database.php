95d5eb9c796c749c51cf7b0bd2a71813443497a0
devimawarni <62754281+devimawarni@users.noreply.github.com>
2020-03-27 22:03:40 +0700

    Update database.php

--- database.php
+++ database.php
@@ -1,1 +1,13 @@
+<?php
 
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbusername = 'devimawarni';
+$dbpassword = 'mawarni4';
+$dbname = 'ubayadb';
+
+$c = new mysqli($dbhost, $dbusername, $dbpassword, $dbnama);
+if ($c->connect_error) {
+  echo "unable to connect, please try again";
+  die();
+}
+?>
